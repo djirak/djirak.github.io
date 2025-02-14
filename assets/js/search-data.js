6bf4a987ea64d6b9ddd5d4bb4d253553650c6471
djirak <48526365+djirak@users.noreply.github.com>
2025-02-14 13:50:28 +0000

    Deploying to gh-pages from @ djirak/djirak.github.io@8221bdbd8077a90590d4797eec39de2880ec2a1e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -410,16 +410,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-acrai-research-day-i-will-give-an-invited-talk-about-social-human-robot-interaction",
+          title: 'ACRAI Research Day: I will give an invited talk about social human-robot interaction...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
