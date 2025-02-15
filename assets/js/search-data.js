a4136130523465db031a0fe9a181e2460fb70731
djirak <48526365+djirak@users.noreply.github.com>
2025-02-15 19:58:52 +0000

    Deploying to gh-pages from @ djirak/djirak.github.io@307c8abe011af476257164e218aa47347aa3e15a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -449,21 +449,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%64%6F%72%65%65%6E.%6A%69%72%61%6B@%75%61%6E%74%77%65%72%70%65%6E.%62%65", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/djirak", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/Doreen Jirak", "_blank");
         },
       },{
         id: 'social-scholar',
@@ -473,11 +473,11 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/LaJiraka", "_blank");
         },
       },{
       id: 'light-theme',
