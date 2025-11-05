016b3c18349fbdb5b826b95467e225e8fde461cb
djirak <48526365+djirak@users.noreply.github.com>
2025-11-05 14:11:45 +0000

    Deploying to gh-pages from @ djirak/djirak.github.io@b8cb7af09aba8345a3a8393583507a1804dfccad 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,9 +416,9 @@ ninja.data = [{
           description: "Implementation of 3D CNNs for human activity recognition at a workbench",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-2-project",
+          title: '2_project',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
